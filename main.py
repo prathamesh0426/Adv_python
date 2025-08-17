9cc431b2ae9a7f5d08b2f36d0d5e0d1b3249184e
prathamesh0426 <>
2025-08-17 22:52:44 +0530

    vari_func

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from adv-python-1130!")
+def simple_interest(p:float, n:float, r:float) -> tuple[float, float]:
+    """
+    This function calculates simple interest and amount
+    """
+    i = (p * n * r)/100
+    a = p+i
+    return i,a
 
+def main():
+    p = float(input("Principal in INR : "))
+    n = float(input("Number of Years : "))
+    r = float(input("Rate of interest in %p.a. : "))
+    i,a = simple_interest(p, n, r)
+    print(f"Simple interest : {i:.2f} INR")
+    print(f"Amount : {a:.2f} INR")
 
 if __name__ == "__main__":
     main()
